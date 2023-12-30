4f5dfa628c1804643237eb0c1f37036ccf6ccaed
danieljou <danielsitepro@gmail.com>
2023-12-30 11:04:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='/sign-in' Component={SignIn} />
+        <Route path='/sign-up' Component={SignUp} />
+        <Route path='/about' Component={About} />
+        <Route path='/profile' Component={Profile} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
